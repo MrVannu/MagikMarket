9f67c8c0afd883cb0aae760ca1c81575db139c72
MrVannu <ilvannu03@gmail.com>
2023-07-24 17:50:32 +0200

    JavaFx Login done

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -1,10 +1,13 @@
 package org.project;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -14,33 +17,58 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Titolo
-        Text title = new Text("Welcome to Magik Market");
+        // Title
+        Text title = new Text("Login");
         title.setFont(Font.font("Arial", FontWeight.BOLD, 24));
 
-        // Bottoni
-        Button button1 = new Button("First");
-        Button button2 = new Button("Second");
+        // Textfields
+        Label usernameLabel = new Label("Username:");
+        Label passwordLabel = new Label("Password:");
 
-        // Layout
-        StackPane layout = new StackPane();
-        layout.getChildren().addAll(title, button1, button2);
+        TextField usernameField = new TextField();
+        PasswordField passwordField = new PasswordField();
 
-        // Scena
-        Scene scene = new Scene(layout, 400, 300);
+        // Button - login
+        Button loginButton = new Button("Login");
+        loginButton.setOnAction(e -> {
+            String username = usernameField.getText();
+            String password = passwordField.getText();
 
-        // Impostazioni della finestra
-        primaryStage.setTitle("Interface JavaFX");
+            // Authentication logic
+            // Db
+
+            System.out.println("Username: " + username);
+            System.out.println("Password: " + password);
+        });
+
+        // Layout della finestra
+        GridPane layout = new GridPane();
+        layout.setPadding(new Insets(20));
+        layout.setHgap(10);
+        layout.setVgap(10);
+
+        layout.add(title, 0, 0, 2, 1);
+        layout.add(usernameLabel, 0, 1);
+        layout.add(usernameField, 1, 1);
+        layout.add(passwordLabel, 0, 2);
+        layout.add(passwordField, 1, 2);
+        layout.add(loginButton, 1, 3);
+
+        Scene scene = new Scene(layout, 300, 200);
+
+        // Options of window
+        primaryStage.setTitle("Login Form");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        APIData testObj = new APIData();
-        testObj.fetchData();
-        double result = testObj.maxAge();
-        System.out.println(result);
-
+        //APIData testObj = new APIData();
+        //testObj.fetchData();
+        //double result = testObj.maxAge();
+        //System.out.println(result);
         launch(args);
     }
 }
+
+

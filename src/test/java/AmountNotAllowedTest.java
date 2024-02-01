871943308c76c6535f9a597112aa3655bff38bc7
MrVannu <ilvannu03@gmail.com>
2024-02-01 14:42:32 +0100

    Typo correction

--- src/test/java/AmountNotAllowedTest.java
+++ src/test/java/AmountNotAllowedTest.java
@@ -1,14 +1,9 @@
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ButtonBar.ButtonData;
 import org.junit.jupiter.api.Test;
-import org.project.AmountNotAllowed;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AmountNotAllowedTest {
-    int test = 0; // Sostituisci con il valore desiderato per il test
+    int test = 0;
 
     @Test
     public void whenAmountIsLessThanOrEqualToZero_thenExceptionIsThrown() {

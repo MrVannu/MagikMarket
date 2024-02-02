c2e1f6132a515d35711591a9803a8d30ed0c51b3
MrVannu <ilvannu03@gmail.com>
2024-02-02 13:39:35 +0100

    Exception Test

--- src/test/java/ExceptionTest.java
+++ src/test/java/ExceptionTest.java
@@ -2,18 +2,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AmountNotAllowedExceptionTest {
-    int test = 0;
+public class ExceptionTest {
 
     @Test
     public void whenAmountIsLessThanOrEqualToZero_thenExceptionIsThrown() {
         Exception exception = assertThrows(RuntimeException.class, () -> {
-            if (test <= 0) {
-                throw new RuntimeException("Amount must be greater than 0");
-            }
+                throw new RuntimeException("Exception thrown");
         });
 
-        String expectedMessage = "Amount must be greater than 0";
+        String expectedMessage = "Exception thrown";
         String actualMessage = exception.getMessage();
 
         assertTrue(actualMessage.contains(expectedMessage));
--- src/test/java/NoCreditExceptionTest.java
+++ src/test/java/NoCreditExceptionTest.java
@@ -8,18 +8,8 @@ public class NoCreditExceptionTest {
 
     int test = 0;
 
-    @Test
-    public void whenNoCredit_thenExceptionIsThrown() {
-        Exception exception = assertThrows(RuntimeException.class, () -> {
-            if (test <= 0){
-                throw new NoCreditException("No credit Exception thrown");
-            }
-        });
 
 
-        String expectedMessage = "No credit Exception thrown";
-        String actualMessage = exception.getMessage();
 
-        assertTrue(actualMessage.contains(expectedMessage));
-    }
+
 }
